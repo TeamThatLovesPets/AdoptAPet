2cc74cafbafaafd3cc691f5037248967898a6375
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-10-09 11:37:34 +0200

    add folder with project

---
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/CatsAndDogsApplication.java
@@ -0,0 +1,12 @@
+package com.teampets.catsAndDogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsAndDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsAndDogsApplication.class, args);
+	}
+}
