40b6ad7b82ccbba3a9b07045025c4047e035ece1
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-10-10 16:32:54 +0200

    Add annotation and comment to Picture class

--- catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/Picture.java
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/Picture.java
@@ -1,4 +1,10 @@
 package com.teampets.catsAndDogs.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pictures")
 public class Picture extends BaseEntity {
+    //TODO: add field with proper type
 }
