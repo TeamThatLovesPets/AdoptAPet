c7f17f61b97e2952c77136217f92fb8204922f1d
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-10-10 16:02:03 +0200

    Add field with annotation to PetType class

--- catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/PetType.java
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/PetType.java
@@ -1,4 +1,13 @@
 package com.teampets.catsAndDogs.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_type")
 public class PetType extends BaseEntity {
+
+    @Column(name = "type_name")
+    private String typeName;
 }
