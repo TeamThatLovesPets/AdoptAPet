97c61733cda0fd6cb7b2e9fcb6d98526bb6cb571
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-10-10 15:57:17 +0200

    Add implementation to PetColour class

--- catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/PetColour.java
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/PetColour.java
@@ -1,4 +1,15 @@
 package com.teampets.catsAndDogs.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pet_colour")
 public class PetColour extends BaseEntity {
+
+    @Column(name = "colour_label")
+    private String colourLabel;
+    //TODO: add constrains/mapping
+    private PetType petType;
 }
