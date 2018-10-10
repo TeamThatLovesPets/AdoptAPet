5b0d572d71906f2bd4ac700b2b537644ffed4cf6
Augustyn Sybidlo <augustyn.sybidlo@gmail.com>
2018-10-10 15:41:38 +0200

    Add fields and annotations to Pet class

--- catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/Pet.java
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/Pet.java
@@ -1,4 +1,37 @@
 package com.teampets.catsAndDogs.model;
 
-public class Pet {
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "pet")
+public class Pet extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "weight")
+    private Float weight;
+    @Column(name = "height")
+    private Float height;
+    @Column(name = "pet_colour")
+    private PetColour petColour;
+    @OneToMany(mappedBy = "") //TODO: add mapping
+    private List<Picture> pictures;
+
+    //TODO: add mapping
+    private User owner;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    @Column(name = "special_marks_description")
+    private String specialMarksDescription;
+    @OneToMany(mappedBy = "") //TODO: add mapping
+    private PetType petType;
+    @Column(name = "localization")
+    private Localization localization;
+
+    //TODO: add mapping
+    private Adoption adoption;
+
+
 }
