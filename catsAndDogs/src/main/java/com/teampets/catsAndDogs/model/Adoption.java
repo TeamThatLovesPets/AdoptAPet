6444a54e79124e759076f76f8e0e79728e3d32b6
augustynSybidlo <augustyn.sybidlo@gmail.com>
2018-10-11 14:29:04 +0200

    Add fields to class Adoption

--- catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/Adoption.java
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/Adoption.java
@@ -1,4 +1,27 @@
 package com.teampets.catsAndDogs.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "adoption")
 public class Adoption extends BaseEntity {
+
+    //TODO: add mapping
+    private AdoptionStatus adoptionStatus;
+    //TODO: add mapping
+    private List<Conversation> conversations;
+    @OneToOne(mappedBy = "") //TODO: add mapping
+    private User petKeeper; // TODO: we need solution to a problem of different
+                            // mapping's for different subclass of User class
+    @OneToOne(mappedBy = "") //TODO: add mapping
+    private User finalAdopter;
+    @OneToOne(mappedBy = "") //TODO: add mapping
+    private Pet pet;
+    @Column(name = "adoption_date")
+    private LocalDateTime adoptionDate;
 }
