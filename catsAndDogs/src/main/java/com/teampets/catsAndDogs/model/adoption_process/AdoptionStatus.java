fe7bf59fd7d817c5deda81c8e46dd880be1ad415
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-10-26 14:18:16 +0200

    add relation between Adoption and Adoption Status

--- catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/adoption_process/AdoptionStatus.java
+++ catsAndDogs/src/main/java/com/teampets/catsAndDogs/model/adoption_process/AdoptionStatus.java
@@ -1,21 +1,23 @@
-//package com.teampets.catsAndDogs.model.adoption_process;
-//
-//import com.teampets.catsAndDogs.model.BaseEntity;
-//import lombok.Builder;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
-//
-//@Setter
-//@Getter
-//@Builder
-//@Entity
-//@Table(name = "adoption_status")
-//public class AdoptionStatus extends BaseEntity {
-//
-//    @Column(name = "status_name")
-//    private String adoptionStatusName;
-//}
+package com.teampets.catsAndDogs.model.adoption_process;
+
+import com.teampets.catsAndDogs.model.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@Entity
+@Table(name = "adoption_status")
+public class AdoptionStatus extends BaseEntity {
+
+    @OneToMany(mappedBy = "adoptionStatus", cascade = {CascadeType.REFRESH,CascadeType.DETACH, CascadeType.MERGE})
+    private List<Adoption> adoptions;
+
+    @Column(name = "status_name")
+    private String adoptionStatusName;
+}
